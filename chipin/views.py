4faab6a020751ad81646335446775dedc70f6038
Lachlan <lachjfisk@gmail.com>
2025-02-26 17:28:15 +1100

    Initial commit

---
+++ chipin/views.py
@@ -0,0 +1,79 @@
+from django.http import HttpResponse 
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.urls import reverse
+from django.conf import settings
+from django.contrib.auth.models import User
+from .forms import GroupCreationForm
+from .models import Group
+import urllib.parse
+
+@login_required
+def home(request):
+    pending_invitations = Group.objects.filter(invited_users__email=request.user.email)
+    return render(request, "chipin/home.html", {'pending_invitations': pending_invitations})
+
+@login_required
+def create_group(request):
+    if request.method == 'POST':
+        form = GroupCreationForm(request.POST, user=request.user)
+        if form.is_valid():
+            group = form.save()
+            messages.success(request, f'Group "{group.name}" created successfully!')
+            return redirect('chipin:group_detail', group_id=group.id)
+    else:
+        form = GroupCreationForm(user=request.user)
+    return render(request, 'chipin/create_group.html', {'form': form})
+
+@login_required
+def group_detail(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    return render(request, 'chipin/group_detail.html', {'group': group})
+
+@login_required
+def delete_group(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    if request.user == group.admin:
+        group.delete()
+        messages.success(request, f'Group "{group.name}" has been deleted.')
+    else:
+        messages.error(request, "You do not have permission to delete this group.")
+    return redirect('chipin:home')
+
+@login_required
+def invite_users(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    users_not_in_group = User.objects.exclude(id__in=group.members.values_list('id', flat=True))
+    if request.method == 'POST':
+        user_id = request.POST.get('user_id')
+        invited_user = get_object_or_404(User, id=user_id)      
+        if invited_user in group.invited_users.all():
+            messages.info(request, f'{invited_user.username} has already been invited.')
+        else:
+            group.invited_users.add(invited_user)
+            messages.success(request, f'Invitation sent to {invited_user.username}.')
+        return redirect('chipin:group_detail', group_id=group.id)  
+    return render(request, 'chipin/invite_users.html', {
+        'group': group,
+        'users_not_in_group': users_not_in_group
+    })
+
+@login_required
+def accept_invite(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    user_id = request.GET.get('user_id')
+    if user_id:
+        invited_user = get_object_or_404(User, id=user_id)
+        if invited_user in group.members.all():
+            messages.info(request, f'{invited_user.username} is already a member of the group "{group.name}".')
+        elif invited_user in group.invited_users.all():
+            group.members.add(invited_user)
+            group.invited_users.remove(invited_user)  # Remove from invited list
+            messages.success(request, f'{invited_user.username} has successfully joined the group "{group.name}".')
+        else:
+            messages.error(request, "You are not invited to join this group.")
+    else:
+        messages.error(request, "Invalid invitation link.")  
+    return redirect('chipin:group_detail', group_id=group.id)
---
+++ users/views_old.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegistrationForm
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been created! You can now log in.")
+            return redirect('users:login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required(login_url='users:login')
+def user(request):
+    return render(request, "users/user.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            next_url = request.GET.get('next', reverse("users:user"))
+            return HttpResponseRedirect(next_url)
+        else:
+            messages.error(request, "Invalid Credentials.")
+    return render(request, "users/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "Successfully logged out.")
+    return redirect('users:login')
